71d8da4c9172eb5bbd4b25a5c2b90508e1c53ea0
alexitm <agarcg25@estudiantes.unileon.es>
2018-06-07 19:45:24 +0200

    Introducido test caja negra

---
+++ src/test/INSO2.peaje.tests/testValidaMatricula.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testValidaMatricula {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+/*
+	@Test
+	public void testVehiculoPeaje() {
+		VehiculoPeaje ve1 = new VehiculoPeaje();
+		Assert.assertTrue(ve1.VehiculoPeaje("9999HJK"));
+		
+		Assert.assertFalse(ve1.VehiculoPeaje("9999 HJK"));
+		
+		Assert.assertFalse(ve1.VehiculoPeaje("9999 999"));
+		
+		Assert.assertFalse(ve1.VehiculoPeaje("9999999"));
+
+		Assert.assertFalse(ve1.VehiculoPeaje("JKHJHJK"));
+
+		Assert.assertFalse(ve1.VehiculoPeaje("JKHJ 999"));
+	}
+*/
+	
+	
+}
